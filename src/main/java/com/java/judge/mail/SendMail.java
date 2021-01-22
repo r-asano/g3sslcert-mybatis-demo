9822b523403b9dba75dad8fde87aff1f40e44e9e
r-asano <asano@jprs.co.jp>
2021-01-22 16:35:51 +0900

    Modify SendMail

--- src/main/java/com/java/judge/mail/SendMail.java
+++ src/main/java/com/java/judge/mail/SendMail.java
@@ -2,6 +2,7 @@ package com.java.judge.mail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
@@ -17,10 +18,12 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.regions.Regions;
@@ -63,41 +66,6 @@ public class SendMail {
     @Value("${spring.mail.password}")
     private String AWS_SECRET;
 
-
-
-
-    // Replace sender@example.com with your "From" address.
-    // This address must be verified with Amazon SES.
-    private static String SENDER = "Sender Name <sender@example.com>";
-
-    // Replace recipient@example.com with a "To" address. If your account
-    // is still in the sandbox, this address must be verified.
-    private static String RECIPIENT = "recipient@example.com";
-
-    // The subject line for the email.
-    private static String SUBJECT = "Customer service contact info";
-
-    // The full path to the file that will be attached to the email.
-    // If you're using Windows, escape backslashes as shown in this variable.
-    private static String ATTACHMENT = "C:\\Users\\sender\\customers-to-contact.xlsx";
-
-    // The email body for recipients with non-HTML email clients.
-    private static String BODY_TEXT = "日本語テスト,\r\n"
-                                        + "Please see the attached file for a list "
-                                        + "of customers to contact.";
-
-    // The HTML body of the email.
-    private static String BODY_HTML = "<html>"
-                                        + "<head></head>"
-                                        + "<body>"
-                                        + "<h1>テストメールです</h1>"
-                                        + "<p>Please see the attached file for a "
-                                        + "list of customers to contact.</p>"
-                                        + "</body>"
-                                        + "</html>";
-
-
-
     /**
      * メール送信
      *
@@ -110,46 +78,52 @@ public class SendMail {
 
         String dateString = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
         String logFileName = prefix + dateString;
-        String getCertLogFileName = "getCert." + logFileName;
-
-        // メールに添付するファイルのオブジェクトを生成
-        FileSystemResource logFileResource = new FileSystemResource(path + logFileName);
-        FileSystemResource getCertLogFileResource = new FileSystemResource(path + getCertLogFileName);
+        //        String getCertLogFileName = "getCert." + logFileName;
+        //
+        //        // メールに添付するファイルのオブジェクトを生成
+        //        FileSystemResource logFileResource = new FileSystemResource(path + logFileName);
+        //        FileSystemResource getCertLogFileResource = new FileSystemResource(path + getCertLogFileName);
 
         // メッセージクラス生成
         MimeMessage message = mailSender.createMimeMessage();
-//        // メッセージ情報をセットするためのヘルパークラスを生成(添付ファイル使用時の第2引数はtrue)
-//        MimeMessageHelper helper = new MimeMessageHelper(mimeMsg, true, ENCODE);
-//
-//        Velocity.setProperty("file.resource.loader.path", "src/main/resources/templates/");
-//        //Velocityの初期化
-//        Velocity.init();
-//
-//        VelocityContext context = new VelocityContext();
-//        context.put("dateString", dateString);
-//        context.put("searchNumber", searchNumber);
-//        context.put("countG3", readMapper.countG3());
-//        context.put("countDV", readMapper.countDV());
-//        context.put("countOV", readMapper.countOV());
-//
-//        StringWriter writer = new StringWriter();
-//        Template template = Velocity.getTemplate("g3mail.vm", ENCODE);
-//        template.merge(context, writer);
-//
-//        helper.setText(writer.toString());
-//        helper.setFrom(FROM);
-//        helper.setTo(TO);
-//        helper.setSubject("■G3サーバ証明書残留状況調査■ (" + dateString + ") -- 淺野稜");
-//
-//        helper.addAttachment(logFileName, logFileResource);
-////        helper.addAttachment(getCertLogFileName, getCertLogFileResource);
 
+        // メッセージ情報をセットするためのヘルパークラスを生成(添付ファイル使用時の第2引数はtrue)
+        //                MimeMessageHelper helper = new MimeMessageHelper(message, true, ENCODE);
 
-        ATTACHMENT = path + logFileName;
+        Velocity.setProperty("file.resource.loader.path", "src/main/resources/templates/");
+        //Velocityの初期化
+        Velocity.init();
 
+        VelocityContext context = new VelocityContext();
+        context.put("dateString", dateString);
+        context.put("searchNumber", searchNumber);
+        context.put("countG3", readMapper.countG3());
+        context.put("countDV", readMapper.countDV());
+        context.put("countOV", readMapper.countOV());
+
+        StringWriter writer = new StringWriter();
+        Template template = Velocity.getTemplate("g3mail.vm", ENCODE);
+        template.merge(context, writer);
+
+        //
+        //        helper.setText(writer.toString());
+        //        helper.setFrom(FROM);
+        //        helper.setTo(TO);
+        //        helper.setSubject("■G3サーバ証明書残留状況調査■ (" + dateString + ") -- 淺野稜");
+        //
+        //        helper.addAttachment(logFileName, logFileResource);
+        ////        helper.addAttachment(getCertLogFileName, getCertLogFileResource);
+
+        String SUBJECT = "■G3サーバ証明書残留状況調査■ (" + dateString + ") -- 淺野 稜";
+
+        // The email body for recipients with non-HTML email clients.
+        String BODY_TEXT = writer.toString();
+
+        String ATTACHMENT1 = path + logFileName;
+        String ATTACHMENT2 = path + "getCert." + logFileName;
 
         // Add subject, from and to lines.
-        message.setSubject(SUBJECT, "ISO-2022-JP");
+        message.setSubject(SUBJECT, ENCODE);
         message.setFrom(new InternetAddress(FROM));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(TO));
 
@@ -161,15 +135,10 @@ public class SendMail {
 
         // Define the text part.
         MimeBodyPart textPart = new MimeBodyPart();
-        textPart.setContent(BODY_TEXT, "text/plain; charset=ISO-2022-JP");
-
-        // Define the HTML part.
-        MimeBodyPart htmlPart = new MimeBodyPart();
-        htmlPart.setContent(BODY_HTML,"text/html; charset=ISO-2022-JP");
+        textPart.setContent(BODY_TEXT, "text/plain; charset=" + ENCODE);
 
         // Add the text and HTML parts to the child container.
         msg_body.addBodyPart(textPart);
-        msg_body.addBodyPart(htmlPart);
 
         // Add the child container to the wrapper object.
         wrap.setContent(msg_body);
@@ -184,15 +153,22 @@ public class SendMail {
         msg.addBodyPart(wrap);
 
         // Define the attachment
-        MimeBodyPart att = new MimeBodyPart();
-        DataSource fds = new FileDataSource(ATTACHMENT);
-        att.setDataHandler(new DataHandler(fds));
-        att.setFileName(fds.getName());
+        MimeBodyPart att1 = new MimeBodyPart();
+        DataSource fds1 = new FileDataSource(ATTACHMENT1);
+        att1.setDataHandler(new DataHandler(fds1));
+        att1.setFileName(fds1.getName());
 
         // Add the attachment to the message.
-        msg.addBodyPart(att);
+        msg.addBodyPart(att1);
 
+        // Define the attachment
+        MimeBodyPart att2 = new MimeBodyPart();
+        DataSource fds2 = new FileDataSource(ATTACHMENT2);
+        att1.setDataHandler(new DataHandler(fds2));
+        att1.setFileName(fds2.getName());
 
+        // Add the attachment to the message.
+        msg.addBodyPart(att2);
 
         // メール送信
         try {
