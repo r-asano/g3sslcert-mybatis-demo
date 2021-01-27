c82834227d3e90a0febdea957eacd51d6e6659b5
r-asano <asano@jprs.co.jp>
2021-01-27 11:43:12 +0900

    One More Velocity

--- src/main/java/com/java/judge/mail/VelocityBean.java
+++ src/main/java/com/java/judge/mail/VelocityBean.java
@@ -1,5 +1,7 @@
 package com.java.judge.mail;
 
+import java.util.Properties;
+
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,16 @@ public class VelocityBean {
 
     @Bean
     public VelocityEngine velocityEngine() {
-        return new VelocityEngine();
+        VelocityEngine Velocity = new VelocityEngine();
+
+        Properties p = new Properties();
+
+        p.setProperty("input.encoding", "ISO-2022-JP");
+        p.setProperty("output.encoding", "ISO-2022-JP");
+
+
+        Velocity.init(p);
+
+        return Velocity;
     }
 }
